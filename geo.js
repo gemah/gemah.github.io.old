38e3d5a2c51b4063da70d60dd285c7edcf20164f
Hian <hianneiva@gmail.com>
2015-05-08 21:52:59 -0300

    TP1 - Página Web
    Implementação v0.2 de mapas

--- geo.js
+++ geo.js
@@ -1,21 +1,55 @@
 var mine = [-19.8634777, -44.001888199999996];
-var markerH;
+var map;
+var posM, posV, pos = null;
+var dd, ds;
 
-function init(){
+function calcRoute(){
 	
-	var mapProp = {
-		center: new google.maps.LatLng(mine[0], mine[1]), 
-		zoom: 14, 
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+	var begin = pos;
+	var end = posM;
+	
+	var req = {
+		origin: begin, destination: end,
+		travelMode: google.maps.TravelMode.DRIVING
 	};
-	var map =  new google.maps.Map(document.getElementById('mapa'), mapProp);
 	
-	markerH = new google.maps.Marker({
-		position: new google.maps.LatLng(mine[0], mine[1]),
-		map: map,
-		title: "Texto"
+	ds.route(req, function(response, status){
+		if(status == google.maps.DirectionsStatus.OK){
+			dd.setDirections(response);
+		}
 	});
 	
 }
 
-window.onload = google.maps.event.addDomListener(window, 'load', init);
+function init(){
+	
+	posM = new google.maps.LatLng(mine[0], mine[1]);
+	ds = new google.maps.DirectionsService();
+	
+	var options = {
+		draggable: true,
+		zoom: 15,
+		center: posM
+	};
+	
+	map = new google.maps.Map(document.getElementById('mapa'), options);
+	
+	var watch = navigator.geolocation.watchPosition(function(position){
+		
+		navigator.geolocation.clearWatch(watch);
+		
+		posV = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		map.setCenter(posV);
+		pos = posV;
+		
+		calcRoute();
+		
+	});
+	
+	dd = new google.maps.DirectionsRenderer();
+	dd.setMap(map);
+	
+	
+}
+
+google.maps.event.addDomListener(window, 'load', init);
