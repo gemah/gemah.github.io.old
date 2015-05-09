6d26a71c09741531988a60f51757adfc05caebd2
Hian <hianneiva@gmail.com>
2015-05-09 00:42:21 -0300

    TP1 - Página Web
    Why doesn't the map works properly edition

--- geo.js
+++ geo.js
@@ -1,66 +1,51 @@
-var homePos = new google.maps.LatLng(-19.862873, -44.00211);
-var map, renderer, servicer,visitorPos;
+var map;
+var homePos = new google.maps.LatLng(-19.862532, -44.001766), visitorPos;
 
-function init(){
+function initialize() {
 
-	var mapOptions = {
-    	draggable: true,
-    	zoom: 15,
-    	center: homePos
-  	};
+  var mapOptions = {
+  	draggable: true,
+    zoom: 15,
+    center: homePos
+  };
+  map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+  
+  /* GEOLOCATOR */
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      visitorPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-	map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: visitorPos,
+        content: 'Sua localização.'
+      });
 
-  	/* Geolocalização */
-
-  	if(navigator.geolocation){
-  		navigator.geolocation.getCurrentPosition(function(position){
-  			visitorPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  			var info = new google.maps.InfoWindow({
-  				map: map, position: visitorPos, content: 'Sua localização'
-  			});
-  			map.setCenter(visitorPos);
-  		}, function(){handleNoGeoLocation(true);});
-  	} else {
-  		handleNoGeoLocation(false);
-  	}
-
-  	/* Desenhador de rota */
-
-	servicer = new google.maps.DirectionsService();
-  	renderer = new google.maps.DirectionsRenderer();
-  	
-	var start, end, req;
-
-	start = visitorPos;
-	end = homePos;
-	req = {origin: start, desination:end, travelMode: google.maps.TravelMode.DRIVING};
-
-	servicer.route(req, function(){
-		if(status == google.maps.DirectionStatus.OK){
-			renderer.setDirections(response);
-		}
-	});
-  	
-  	renderer.setMap(map);
-
+      map.setCenter(visitorPos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
 }
 
-function handleNoGeoLocation(errorFlag){
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Serviço de Geolocalização falhou.';
+  } else {
+    var content = 'Seu navegador não suporta geolocalização.';
+  }
 
-	var content, options, info;
+  var options = {
+    map: map,
+    position: homePos,
+    content: content
+  };
 
-	if(errorFlag){
-		content = 'Erro: Serviço de Geolocalização falhou.';
-	} else {
-		content = 'Erro: Seu navegador não suporta Geolocalização!'
-	}
-
-	options = {map: map, position: homePos, content: content};
-
-	info = new google.maps.InfoWindow(options);
-	map.setCenter(info.position);
-
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', initialize);
